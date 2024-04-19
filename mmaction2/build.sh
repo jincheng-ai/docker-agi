962d48e2b40217b1d32ad01a3c1da692f5028de1
Jincheng Shu <ai_jcshu@163.com>
2024-04-19 10:04:45 +0800

    修改mmaction2源码构建方式

--- mmaction2/build.sh
+++ mmaction2/build.sh
@@ -9,4 +9,10 @@ else
     echo "mmaction2版本: $version"
 fi
 
+if [ ! -d "mmaction2" ];then
+    echo "git clone mmaction2"
+else
+    echo "清除已存在的mmaction2文件夹，重新下载"
+fi
+git clone -b $version https://github.com/open-mmlab/mmaction2.git mmaction2
 docker build --build-arg version=$version -t jincheng_jcs/mmaction2:$version . |& tee ./build.log 2>&1
